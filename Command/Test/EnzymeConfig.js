d51cd6b5cf554ba85ccb5dfdc61f4be7d4e1313a
dr.sina121@gmail.com <khodabandehloo.sina@gmail.com>
2021-07-25 04:49:53 +0430

    refactor: disable test globals

--- Command/Test/EnzymeConfig.js
+++ Command/Test/EnzymeConfig.js
@@ -1,10 +1,4 @@
-import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({adapter: new Adapter()});
-
-global.React = React;
-global.mount = Enzyme.mount;
-global.render = Enzyme.render;
-global.shallow = Enzyme.shallow;
--- jest.config.js
+++ jest.config.js
@@ -17,5 +17,6 @@ export default async () => {
         globals: {
             NODE_ENV: "test"
         },
+        injectGlobals: false
     };
 };
