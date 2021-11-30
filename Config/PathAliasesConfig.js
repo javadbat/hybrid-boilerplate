72c92cd3f6aa1ed6e2cd01e27fe3dfa7a4b7b390
javadbat <javadbat@gmail.com>
2021-11-30 15:52:02 +0330

    add some comment

--- Config/PathAliasesConfig.js
+++ Config/PathAliasesConfig.js
@@ -1,7 +1,7 @@
 import path from "path";
 import { createRequire } from "module";
 import { generalConfigServer } from "./GeneralConfigServer.js";
-
+// WARNIING: do not use this file iinsiide client code
 /* replace `require` with `import` after node supports `json` import */
 const require = createRequire(import.meta.url);
 const jsconfig = require("../jsconfig.json");
@@ -12,10 +12,12 @@ const aliases = jsconfig.compilerOptions.paths;
 const removeStars = (path) => path.slice(-2) === "/*" ? path.slice(0, -2) : path;
 
 /**
+ * convert jsconfig paths to webpack aliases
  * @return {Record<string, string>}
  */
 const getResolvedAliases = () => {
     const result = {};
+    // eslint-disable-next-line guard-for-in
     for (const key in aliases) {
         const aliasKey = removeStars(key);
         const aliasValue = removeStars(aliases[key][0]);
@@ -26,4 +28,7 @@ const getResolvedAliases = () => {
 };
 export const resolvedAliases = getResolvedAliases();
 
+/**
+ * convert jsconfig paths to jest path aliases
+ */
 export const jestAliasMaps = Object.entries(resolvedAliases).reduce((acc, [alias, dir]) => ({ ...acc, ...({ [`^${alias}(.*)$`]: dir + "$1" }) }), {});
