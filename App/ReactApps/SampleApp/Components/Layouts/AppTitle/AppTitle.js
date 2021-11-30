3651b368659f150756ea8734757a60d539ca1100
javadbat <javadbat@gmail.com>
2021-11-30 13:43:50 +0330

    fixx base path problem

--- App/ReactApps/SampleApp/Components/Layouts/AppTitle/AppTitle.js
+++ App/ReactApps/SampleApp/Components/Layouts/AppTitle/AppTitle.js
@@ -2,8 +2,8 @@ import React from "react";
 import generalConfig from "@config/GeneralConfig";
 
 export const SampleAppTitle = () => (
-  <div className="project-title">
-    <h1>Sample ReactJs App</h1>
-    <h5>mode: {generalConfig.env}</h5>
-  </div>
+    <div className="project-title">
+        <h1>Sample ReactJs App</h1>
+        <h5>mode: {generalConfig.env}</h5>
+    </div>
 );
