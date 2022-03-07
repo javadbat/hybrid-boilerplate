2a4ffebe5b507a93e5ca1e1afae28a1bc4ab1400
javadbat <javadbat@gmail.com>
2022-03-07 19:19:01 +0330

    add is development tag in service worker

--- App/ReactApps/SampleApp/PWA/ServiceWorker.js
+++ App/ReactApps/SampleApp/PWA/ServiceWorker.js
@@ -6,7 +6,7 @@ import { CacheFirst } from 'workbox-strategies';
 //node app version
 // ATTENTION: use appVersion in cache name you want to delete cache when app version change
 const appVersion = `app-ver${process.env.npm_package_version}` ;
-
+const isDevelopment = process.env.NODE_ENV === 'development';
 //config
 setCacheNameDetails({
     prefix: 'sample-app',
@@ -16,46 +16,53 @@ setCacheNameDetails({
 });
 // Use with precache injection
 // will precache all files in globDirectory that match globPatterns
-const machineGeneratedPreCacheList = self.__WB_MANIFEST;
-const htmlFilePath = '/sample-app';
-const htmlFileCacheEntity = { url: htmlFilePath, revision: '100001' };
-const manifestCacheEntity = { url: '/sample-app/manifest.json', revision: '100002' };
-const precachedStyles = [
-    { url: '/App/Assets/Css/Common/Layout.css', revision: '100003' },
-    { url: '/App/Assets/Css/Common/General.css', revision: '100004' }
-];
-let preCacheList = [htmlFileCacheEntity, manifestCacheEntity, ...precachedStyles, ...machineGeneratedPreCacheList];
-precacheAndRoute(preCacheList, {
-    // Ignore all URL parameters. /about.html?q=1 => /about.html
-    ignoreURLParametersMatching: [/.*/],
-    directoryIndex: null,
-});
+if(!isDevelopment){
+    const machineGeneratedPreCacheList = self.__WB_MANIFEST;
+    const htmlFilePath = '/sample-app';
+    const htmlFileCacheEntity = { url: htmlFilePath, revision: '100001' };
+    const manifestCacheEntity = { url: '/sample-app/manifest.json', revision: '100002' };
+    const precachedStyles = [
+        { url: '/App/Assets/Css/Common/Layout.css', revision: '100003' },
+        { url: '/App/Assets/Css/Common/General.css', revision: '100004' }
+    ];
+    let preCacheList = [htmlFileCacheEntity, manifestCacheEntity, ...precachedStyles, ...machineGeneratedPreCacheList];
+    precacheAndRoute(preCacheList, {
+        // Ignore all URL parameters. /about.html?q=1 => /about.html
+        ignoreURLParametersMatching: [/.*/],
+        directoryIndex: null,
+    });
+}
+
 
 // cache routes
-registerRoute(
-    ({ request }) => {
-        //check url for more specific cache rules
-        //const reqUrl = request.url;
-        const isImage = request.destination === 'image';
-        return isImage;
-    },
-    new CacheFirst({
-        cacheName: 'app-image-cache',
-    })
-);
+if(!isDevelopment){
+    registerRoute(
+        ({ request }) => {
+            //check url for more specific cache rules
+            //const reqUrl = request.url;
+            const isImage = request.destination === 'image';
+            return isImage;
+        },
+        new CacheFirst({
+            cacheName: 'app-image-cache',
+        })
+    );
+}
 
-registerRoute(
-    ({ request }) => {
-        //check url for more specific cache rules
-        //const reqUrl = request.url;
-        const isImage = request.destination === 'font';
-        return isImage;
-    },
-    new CacheFirst({
-        cacheName: 'app-font-cache',
-    })
-);
-
+//caceh fonts
+if(!isDevelopment){
+    registerRoute(
+        ({ request }) => {
+            //check url for more specific cache rules
+            //const reqUrl = request.url;
+            const isImage = request.destination === 'font';
+            return isImage;
+        },
+        new CacheFirst({
+            cacheName: 'app-font-cache',
+        })
+    );
+}
 // Clean up caches in activate event to ensure no pages
 // are using the old caches.
 self.addEventListener('activate', (event) => {
