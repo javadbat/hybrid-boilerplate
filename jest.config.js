5d343fdc370b8f4cf4c7e67f30ea83ecc1516c06
javadbat <javadbat@gmail.com>
2023-11-27 00:49:23 +0330

    feat: add husky

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,21 @@
 import path from "path";
-import { jestAliasMaps } from "./Config/PathAliasesConfig.js";
-import { generalConfigServer } from "./Config/GeneralConfigServer.js";
+import { jestAliasMaps } from "./config/path-aliases-config.js";
+import { generalConfigServer } from "./config/general-config-server.js";
 
 export default async () => {
     return {
         verbose: true,
         testEnvironment:'jsdom',
         transform: {
-            '\\.jsx?$': ['babel-jest', { configFile: path.join(generalConfigServer.basePath, 'Command', 'Test', 'BabelConfig.js') }]
+            '\\.jsx?$': ['babel-jest', { configFile: path.join(generalConfigServer.basePath, 'command', 'test', 'babel-config.js') }]
         },
         moduleNameMapper: {
-            '.*\\.(css|less|styl|scss|sass)$': path.join(generalConfigServer.basePath, 'Command', 'Test', 'mocks', 'styleMocks.js'),
+            '.*\\.(css|less|styl|scss|sass)$': path.join(generalConfigServer.basePath, 'command', 'test', 'mocks', 'style-mocks.js'),
             '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            path.join(generalConfigServer.basePath, 'Command', 'Test', 'mocks', 'mediaMocks.js'),
+            path.join(generalConfigServer.basePath, 'command', 'test', 'mocks', 'media-mocks.js'),
             ...jestAliasMaps,
         },
-        setupFiles: ['raf/polyfill', path.join(generalConfigServer.basePath, 'Command', 'Test', 'EnzymeConfig.js')],
+        setupFiles: ['raf/polyfill', path.join(generalConfigServer.basePath, 'command', 'test', 'enzyme-config.js')],
         globals: {
             NODE_ENV: "test"
         },
