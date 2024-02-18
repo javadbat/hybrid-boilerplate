ff072d8aaaa2fb01ceaab235f814d83e167a7437
javadbat <javadbat@gmail.com>
2024-02-18 11:10:47 +0330

    fix simple test suit

--- command/test/test-command.js
+++ command/test/test-command.js
@@ -4,7 +4,7 @@ import path from "path";
 console.log(chalk.green('start running JEST tests') );
 const jestScriptPath = path.join('node_modules', 'jest', 'bin', 'jest.js');
 //show test coverage results at the end
-const showCovarageReportParam = "--collect-coverage";
+const showCoverageReportParam = "--collect-coverage";
 //bailing mean test exit when 1 number of fail happen --bail or -b
 const bailParam = `--bail`;
 // --detectOpenHandles handle force exists process so only use it in production readmode:https://jestjs.io/docs/cli
@@ -17,5 +17,5 @@ const watchParam = "--watch";
 const NoCache = '--no-cache';
 
 const userArgs = process.argv.slice(2);
-const params = [showCovarageReportParam, bailParam];
+const params = [showCoverageReportParam, bailParam,detectOpenHandle];
 const testProcess = spawn(`node`,['--experimental-vm-modules', jestScriptPath, ...params, ...userArgs],{shell:true,stdio:'inherit'});
