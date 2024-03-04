3f3e82a744b89bb324b8983154b91bae96bb3db8
javadbat <javadbat@gmail.com>
2024-03-04 16:43:58 +0330

    update click outside

--- app/react-apps/utils/custom-hooks/useClickOutside.ts
+++ app/react-apps/utils/custom-hooks/useClickOutside.ts
@@ -1,7 +1,7 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, RefObject } from "react";
 
 // check if user clicks outside of a component to hide it
-export const useClickOutside = (initialIsVisible:boolean, shouldCloseOnSelection = false) => {
+export const useHandleClickOutside = (initialIsVisible:boolean, shouldCloseOnSelection = false) => {
     const [isComponentVisible, setIsComponentVisible] = useState(initialIsVisible);
     const ref = useRef<HTMLElement>(null);
 
@@ -22,4 +22,30 @@ export const useClickOutside = (initialIsVisible:boolean, shouldCloseOnSelection
     }, []);
 
     return { ref, isComponentVisible, setIsComponentVisible };
+};
+// check if user clicks outside of a component to hide it
+export const useClickOutside = (ref:RefObject<HTMLElement>, onClickOutside:()=>void, exceptions:RefObject<HTMLElement>[]) => {
+
+    const handleClickOutside = (event:MouseEvent) => {
+        if (ref.current && !ref.current.contains(event.target as Node)) {
+            let isExceptionContains = false;
+            exceptions.forEach((ex)=>{
+                if(ex.current?.contains(event.target as Node)){
+                    isExceptionContains = true;
+                }
+            })
+            setTimeout(() => {
+                if (ref?.current && !isExceptionContains){
+                    onClickOutside();
+                }
+            }, 0);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside, true);
+        return () => {
+            document.removeEventListener("click", handleClickOutside, true);
+        };
+    }, [ref,exceptions]);
 };
